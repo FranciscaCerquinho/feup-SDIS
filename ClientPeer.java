6f158c4735b291ec447ad6a746414f55969530c1
VenturaPereira <venturasousapereira@hotmail.com>
2018-03-16 17:39:03 +0000

    Rmi working

---
+++ ClientPeer.java
@@ -0,0 +1,24 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class ClientPeer {
+	
+	private ClientPeer(){}
+	
+	
+	public static void main(String[] args){
+			
+		try{
+			Registry registry = LocateRegistry.getRegistry(1099);
+			RMIinterface stub = (RMIinterface) registry.lookup("trial");
+			String response = stub.helloWorld();
+			System.out.println("response: " + response);
+		}catch(Exception e){
+			System.err.println("Client exception: " + e.toString());
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+}
---
+++ Peer.java
@@ -0,0 +1,45 @@
+import java.net.MulticastSocket;
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+
+public class Peer implements RMIinterface{
+	
+	private static int peerID;
+	private static MulticastSocket socket;
+	
+	
+	public Peer(){}
+	
+	public String helloWorld(){
+		return "Hello, world!";
+	}
+	public static void main(String[] args){
+		
+		System.setProperty()
+		
+		try{
+			
+			Peer obj = new Peer();
+			RMIinterface stub = (RMIinterface) UnicastRemoteObject.exportObject(obj,0);
+			
+			
+			Registry registry = LocateRegistry.getRegistry();
+			registry.bind("trial", stub);
+			
+			System.err.println("Server ready");	
+		}catch(Exception e){
+			System.err.println("Server exception: " + e.toString());
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	
+	
+	
+}
